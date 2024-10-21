85c40cc54e564497f81c62519533c6d3dc05899d
QuocSon98 <sonnqse182727@fpt.edu.vn>
2024-10-21 19:22:11 +0700

    fix consign care status

--- src/page/manageConsignCare/index.jsx
+++ src/page/manageConsignCare/index.jsx
@@ -87,7 +87,7 @@ const ManageConsignCare = () => {
                                 <tr key={order.id} className={styles.row}>
                                     <td className={styles.textLeft}>{order.id}</td>
                                     <td className={styles.textLeft}>{order.orderDate}</td>
-                                    <td className={styles.textRight}>{order.totalPrice.toLocaleDateString('vi-VN')}</td>
+                                    <td className={styles.textRight}>{order.totalPrice.toLocaleString('vi-VN')}</td>
                                     <td className={styles.textCenter}>
                                         <button
                                             className={styles.button1}
