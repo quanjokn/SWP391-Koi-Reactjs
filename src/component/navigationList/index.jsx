9f2bfb512076d2e0be93273e51122551703e712b
QuocSon98 <sonnqse182727@fpt.edu.vn>
2024-10-21 22:50:22 +0700

    fix show profile

--- src/component/navigationList/index.jsx
+++ src/component/navigationList/index.jsx
@@ -7,6 +7,7 @@ const NavigationList = () => {
     const navigate = useNavigate();
     const { logout } = useContext(UserContext);
     const location = useLocation();
+    const { user } = useContext(UserContext);
 
     const handleLogout = () => {
         logout(); // Gọi hàm logout
@@ -17,31 +18,80 @@ const NavigationList = () => {
     const isActive = (path) => (location.pathname === path ? styles.active : '');
 
     return (
-        <div className="col-md-3">
-            <ul className={`${styles.listGroup} list-group`}>
-                <li className={`${styles.listGroupItem} list-group-item`}>
-                    <Link to="/">Trang chủ</Link>
-                </li>
-                <li className={`${styles.listGroupItem} list-group-item ${isActive('/order-list')}`}>
-                    <Link to="/order-list">Đơn hàng</Link>
-                </li>
-                <li className={`${styles.listGroupItem} list-group-item ${isActive('/order-list-consign-care')}`}>
-                    <Link to="/order-list-consign-care">Đơn ký gửi chăm sóc</Link>
-                </li>
-                <li className={`${styles.listGroupItem} list-group-item ${isActive('/order-list-consign-sell')}`}>
-                    <Link to="/order-list-consign-sell">Đơn ký gửi để bán</Link>
-                </li>
-                <li className={`${styles.listGroupItem} list-group-item ${isActive('/tai-khoan')}`}>
-                    <Link to="/tai-khoan">Trang tài khoản</Link>
-                </li>
-                <li className={`${styles.listGroupItem} list-group-item ${isActive('/doi-mat-khau')}`}>
-                    <Link to="/doi-mat-khau">Thay đổi mật khẩu</Link>
-                </li>
-                <li className={`${styles.listGroupItem} list-group-item`}>
-                    <Link to="" onClick={handleLogout}>Đăng xuất</Link>
-                </li>
-            </ul>
-        </div>
+        <>
+            {(user?.role === 'Customer' || !user) && (
+                <div className="col-md-3">
+                    {/* Hiển thị menu và giỏ hàng cho Customer */}
+
+
+                    <ul className={`${styles.listGroup} list-group`}>
+                        <li className={`${styles.listGroupItem} list-group-item`}>
+                            <Link to="/">Trang chủ</Link>
+                        </li>
+                        <li className={`${styles.listGroupItem} list-group-item ${isActive('/order-list')}`}>
+                            <Link to="/order-list">Đơn hàng</Link>
+                        </li>
+                        <li className={`${styles.listGroupItem} list-group-item ${isActive('/order-list-consign-care')}`}>
+                            <Link to="/order-list-consign-care">Đơn ký gửi chăm sóc</Link>
+                        </li>
+                        <li className={`${styles.listGroupItem} list-group-item ${isActive('/order-list-consign-sell')}`}>
+                            <Link to="/order-list-consign-sell">Đơn ký gửi để bán</Link>
+                        </li>
+                        <li className={`${styles.listGroupItem} list-group-item ${isActive('/tai-khoan')}`}>
+                            <Link to="/tai-khoan">Trang tài khoản</Link>
+                        </li>
+                        <li className={`${styles.listGroupItem} list-group-item ${isActive('/doi-mat-khau')}`}>
+                            <Link to="/doi-mat-khau">Thay đổi mật khẩu</Link>
+                        </li>
+                        <li className={`${styles.listGroupItem} list-group-item`}>
+                            <Link to="" onClick={handleLogout}>Đăng xuất</Link>
+                        </li>
+                    </ul>
+                </div>
+            )}
+
+            {/* Hiển thị menu và giỏ hàng cho Customer */}
+            {(user?.role === 'Staff' || !user) && (
+                <div className="col-md-3">
+                    <ul className={`${styles.listGroup} list-group`}>
+                        <li className={`${styles.listGroupItem} list-group-item`}>
+                            <Link to="/">Trang chủ</Link>
+                        </li>
+                        <li className={`${styles.listGroupItem} list-group-item ${isActive('/tai-khoan')}`}>
+                            <Link to="/tai-khoan">Trang tài khoản</Link>
+                        </li>
+                        <li className={`${styles.listGroupItem} list-group-item ${isActive('/doi-mat-khau')}`}>
+                            <Link to="/doi-mat-khau">Thay đổi mật khẩu</Link>
+                        </li>
+                        <li className={`${styles.listGroupItem} list-group-item`}>
+                            <Link to="" onClick={handleLogout}>Đăng xuất</Link>
+                        </li>
+                    </ul>
+
+                </div>
+            )}
+
+            {/* Hiển thị menu và giỏ hàng cho Customer */}
+            {(user?.role === 'Manager' || !user) && (
+                <div className="col-md-3">
+                    <ul className={`${styles.listGroup} list-group`}>
+                        <li className={`${styles.listGroupItem} list-group-item`}>
+                            <Link to="/">Trang chủ</Link>
+                        </li>
+                        <li className={`${styles.listGroupItem} list-group-item ${isActive('/tai-khoan')}`}>
+                            <Link to="/tai-khoan">Trang tài khoản</Link>
+                        </li>
+                        <li className={`${styles.listGroupItem} list-group-item ${isActive('/doi-mat-khau')}`}>
+                            <Link to="/doi-mat-khau">Thay đổi mật khẩu</Link>
+                        </li>
+                        <li className={`${styles.listGroupItem} list-group-item`}>
+                            <Link to="" onClick={handleLogout}>Đăng xuất</Link>
+                        </li>
+                    </ul>
+
+                </div>
+            )}
+        </>
     );
 };
 
